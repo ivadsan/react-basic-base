e9658df5991b1ed7334a6db0cb75e6489e1bbb6c
Iván D. Sánchez <cosmosoft.soluciones@gmail.com>
2020-06-12 01:00:09 -0500

    [base] basic base for react app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
+import './styles/Global.scss'
 
 export const App = () => {
   return (
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
 module.exports = {
   output: {
-    filename: 'app.bundle.js'
+    filename: 'js/app.bundle.js'
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'your title',
       template: 'public/index.html'
+    }),
+
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[hash].css',
+      chunkFilename: 'css/[id].[hash].css'
     })
   ],
   module: {
@@ -18,6 +25,27 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.(s*)css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: '[hash].[ext]',
+            outputPath: 'assets'
           }
         }
       }
