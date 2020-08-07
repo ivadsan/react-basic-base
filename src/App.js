5b8dbc34485fe8ca2f95c780ec8bd3faac10ac90
@cosmosoftroot <cosmosoft.soluciones@gmail.com>
2020-08-07 18:51:56 -0500

    [dependencies] update with audit fix

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import { Home } from './pages/Home'
 import './styles/Global.scss'
 
 export const App = () => {
+  console.log(process.env)
   return (
     <BrowserRouter>
+    {/* {`NODE_ENV: ${process.env.NODE_ENV}`} */}
+    {`BASE_URL: ${process.env.BASE_URL}`}
+    {`PORT: ${process.env.PORT}`}
       <Switch>
         <Route exact path='/' component={Home} />
       </Switch>
